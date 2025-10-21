93db7870603d2a9d673553652ea28f1e5057fb5f
Villo05 <nemethvillo.2005@gmail.com>
2025-10-21 15:01:21 +0200

    kesz

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ db = osztalyzatok.elegtelenek_szama(szamok)
 print("Elégtelen jegyek száma:", db)
 
 osztalyzatok.atlag(szamok)
-<<<<<<< HEAD
 osztalyzatok.masodik(szamok)
-=======
 ittcsinalom.harmadik(szamok)
->>>>>>> 6c03f20096aa3ec486d07a71789b5f65de0c22ac
